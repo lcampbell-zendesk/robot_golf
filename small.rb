e024565465ff3d9bc871a3301374b1c6df4b265c
Logan Campbell <lcampbellmcpherson@zendesk.com>
2022-07-04 22:53:32 +1000

    join lines and trim more whitespace

--- small.rb
+++ small.rb
@@ -1,46 +1,25 @@
 #!/usr/bin/ruby
-B = 0...5
+B=0...5
 N="NORTH"
 S="SOUTH"
 E="EAST"
 W="WEST"
 P="PLACE"
-puts (ARGF.read
-.split("\n")
-.map{|l|l.split(/[ ,]/) }
-.drop_while{|c|c.first!=P}
-.reduce({x:nil,y:nil,f:nil,out:[]})do|s, l|
-n,*a=l
-case n
-when P
+puts (ARGF.read.split("\n").map{|l|l.split(/[ ,]/)}.drop_while{|c|c.first!=P}.reduce({x:nil,y:nil,f:nil,out:[]})do|s,l|n,*a=l
+case n when P
 x,y,f=a
 z=x.to_i
 t=y.to_i
 next s unless B.include?(z)and B.include? t
 s.merge({x:z,y:t,f:f})
 when"LEFT"
-s[:f]={
-N=>W,
-W=>S,
-S=>E,
-E=>N,
-}[s[:f]]
+s[:f]={N=>W,W=>S,S=>E,E=>N}[s[:f]]
 s
 when"RIGHT"
-s[:f]={
-N=>E,
-E=>S,
-S=>W,
-W=>N,
-}[s[:f]]
+s[:f]={N=>E,E=>S,S=>W,W=>N}[s[:f]]
 s
 when"MOVE"
-z,t={
-N=>[+0,+1],
-S=>[+0,-1],
-E=>[+1,+0],
-W=>[-1,+0],
-}[s[:f]]
+z,t={N=>[+0,+1],S=>[+0,-1],E=>[+1,+0],W=>[-1,+0]}[s[:f]]
 c=s[:x]+z
 u=s[:y]+t
 next s unless B.include?(c)and B.include? u
@@ -48,7 +27,4 @@ s.merge({x:c,y:u})
 when"REPORT"
 s[:out]<<[s[:x],s[:y],s[:f]].join(',')
 s
-else
-fail
-end
-end)[:out]
+else fail end end)[:out]
